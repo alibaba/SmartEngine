36df160c7706cf51a9d4e2aac241792736c09508
haijun.ghj <haijun.ghj@alibaba-inc.com>
2017-03-14 22:47:54 +0800

    add test

---
+++ storage.memory/src/test/java/com/alibaba/smart/framework/engine/modules/bpmn/LotteryDrarProcessTest.java
@@ -0,0 +1,46 @@
+package com.alibaba.smart.framework.engine.modules.bpmn;
+
+import com.alibaba.smart.framework.engine.SmartEngine;
+import com.alibaba.smart.framework.engine.configuration.ProcessEngineConfiguration;
+import com.alibaba.smart.framework.engine.configuration.impl.DefaultProcessEngineConfiguration;
+import com.alibaba.smart.framework.engine.impl.DefaultSmartEngine;
+import com.alibaba.smart.framework.engine.model.assembly.ProcessDefinition;
+import com.alibaba.smart.framework.engine.model.instance.ProcessInstance;
+import com.alibaba.smart.framework.engine.service.command.ProcessCommandService;
+import com.alibaba.smart.framework.engine.service.command.RepositoryCommandService;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LotteryDrarProcessTest {
+
+
+	@Test
+	public void testExclusive() throws Exception {
+	    ProcessEngineConfiguration processEngineConfiguration  = new DefaultProcessEngineConfiguration();
+
+		SmartEngine smartEngine = new DefaultSmartEngine();
+		smartEngine.init(processEngineConfiguration);
+
+		RepositoryCommandService repositoryService = smartEngine
+				.getRepositoryCommandService();
+		ProcessDefinition processDefinition = repositoryService
+				.deploy(" lottery_draw_process.xml");
+        Assert.assertEquals(18, processDefinition.getProcess().getElements().size());
+
+		ProcessCommandService processService = smartEngine.getProcessCommandService();
+		Map<String, Object> request = new HashMap<String, Object>();
+		request.put("input", 2);
+		ProcessInstance processInstance = processService.start(
+				processDefinition.getId(), processDefinition.getVersion(),
+				request);
+
+		Assert.assertNotNull(processInstance);
+	}
+
+
+
+
+}
