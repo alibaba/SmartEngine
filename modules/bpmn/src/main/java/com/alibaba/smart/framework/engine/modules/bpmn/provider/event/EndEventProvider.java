a53a51a64de7526126a6ce9542b73fea6e3df391
dongdong.zdd <dongdong.zdd@alibaba-inc.com>
2017-05-28 11:31:11 +0800

    fix

--- modules/bpmn/src/main/java/com/alibaba/smart/framework/engine/modules/bpmn/provider/event/EndEventProvider.java
+++ modules/bpmn/src/main/java/com/alibaba/smart/framework/engine/modules/bpmn/provider/event/EndEventProvider.java
@@ -1,12 +1,20 @@
 package com.alibaba.smart.framework.engine.modules.bpmn.provider.event;
 
+import com.alibaba.smart.framework.engine.exception.EngineException;
 import com.alibaba.smart.framework.engine.extensionpoint.registry.ExtensionPointRegistry;
+import com.alibaba.smart.framework.engine.invocation.Invoker;
 import com.alibaba.smart.framework.engine.modules.bpmn.assembly.event.EndEvent;
 import com.alibaba.smart.framework.engine.modules.bpmn.provider.process.AbstractBpmnActivityProvider;
 import com.alibaba.smart.framework.engine.provider.ActivityProvider;
 import com.alibaba.smart.framework.engine.pvm.PvmActivity;
 
 public class EndEventProvider extends AbstractBpmnActivityProvider<EndEvent> implements ActivityProvider<EndEvent> {
+
+
+    @Override
+    protected Invoker createEventInvoker(String event) {
+        throw new EngineException("assign acitivy is on before");
+    }
 
     public EndEventProvider(ExtensionPointRegistry extensionPointRegistry, PvmActivity runtimeActivity) {
         super(extensionPointRegistry, runtimeActivity);
