50315e569671249d37d55575929ce5f4f11f9c1e
ziran.pengzr <ziran.pengzr@alibaba-inc.com>
2017-08-14 11:38:31 +0800

    fix unit test

--- storage.memory/src/test/java/com/alibaba/smart/framework/engine/modules/bpmn/ReceiveTaskParallelGatewayTest.java
+++ storage.memory/src/test/java/com/alibaba/smart/framework/engine/modules/bpmn/ReceiveTaskParallelGatewayTest.java
@@ -26,7 +26,7 @@ import static org.junit.Assert.assertTrue;
 
 public class ReceiveTaskParallelGatewayTest {
 
-    //@Test
+    @Test
     public void testParallelGateway() throws Exception {
         ProcessEngineConfiguration processEngineConfiguration = new DefaultProcessEngineConfiguration();
 
@@ -67,24 +67,36 @@ public class ReceiveTaskParallelGatewayTest {
         List<ExecutionInstance> executionInstanceList =executionQueryService.findActiveExecution(processInstance.getInstanceId());
         assertEquals(2, executionInstanceList.size());
 
-
         ExecutionInstance firstExecutionInstance = executionInstanceList.get(0);
+        String firstActivityId=firstExecutionInstance.getActivityId();
+
         ExecutionInstance secondExecutionInstance = executionInstanceList.get(1);
+        String secondActivityId=secondExecutionInstance.getActivityId();
 
-
-
-        assertTrue("theTask1".equals(firstExecutionInstance.getActivityId()) ||"theTask2".equals(firstExecutionInstance.getActivityId()));
-        assertTrue("theTask1".equals(secondExecutionInstance.getActivityId()) ||"theTask2".equals(secondExecutionInstance.getActivityId()));
-
-
+        assertTrue(("theTask1".equals(firstActivityId) && "theTask2".equals(secondActivityId)) || ("theTask1".equals(secondActivityId) && "theTask2".equals(firstActivityId)));
 
         //完成两个任务节点创建,已经进入fork节点后面的数据。完成其中一个节点的驱动。
         processInstance = executionCommandService.signal(firstExecutionInstance.getInstanceId(), null);
 
+        String runningActivityId=secondActivityId;
+
         executionInstanceList =executionQueryService.findActiveExecution(processInstance.getInstanceId());
+        assertEquals(2, executionInstanceList.size());
+
         firstExecutionInstance = executionInstanceList.get(0);
-        assertEquals(1, executionInstanceList.size());
-        assertTrue("theTask1".equals(firstExecutionInstance.getActivityId()) ||"theTask2".equals(firstExecutionInstance.getActivityId()));
+        firstActivityId=firstExecutionInstance.getActivityId();
+
+        secondExecutionInstance = executionInstanceList.get(1);
+        secondActivityId=secondExecutionInstance.getActivityId();
+
+        ExecutionInstance runningExecutionInstance;
+        if(firstActivityId.equals("join")){
+            assertEquals(runningActivityId,secondActivityId);
+            runningExecutionInstance=secondExecutionInstance;
+        }else{
+            assertTrue(runningActivityId.equals(firstActivityId) ||"join".equals(secondActivityId));
+            runningExecutionInstance=firstExecutionInstance;
+        }
 
         activityInstances =  activityQueryService.findAll(processInstance.getInstanceId());
         Assert.assertNotNull(activityInstances);
@@ -95,15 +107,12 @@ public class ReceiveTaskParallelGatewayTest {
 
         //完成后面一个节点的驱动。
         request.put("input", 11);
-        processInstance = executionCommandService.signal(firstExecutionInstance.getInstanceId(), request);
+        processInstance = executionCommandService.signal(runningExecutionInstance.getInstanceId(), request);
 
         executionInstanceList =executionQueryService.findActiveExecution(processInstance.getInstanceId());
         firstExecutionInstance = executionInstanceList.get(0);
         assertEquals(1, executionInstanceList.size());
-
-        //assertTrue("theTask3".equals(firstExecutionInstance.getActivityId()));
-
-
+        assertTrue("theTask3".equals(firstExecutionInstance.getActivityId()));
 
         processInstance = executionCommandService.signal(firstExecutionInstance.getInstanceId(), request);
         Assert.assertNotNull(processInstance.getCompleteDate());
