54eab3077d836920e76e659f9772681ef40a0678
dongdong.zdd <dongdong.zdd@alibaba-inc.com>
2018-05-15 17:16:50 +0800

    fix end

--- core/runtime/src/main/java/com/alibaba/smart/framework/engine/instance/impl/DefaultProcessInstance.java
+++ core/runtime/src/main/java/com/alibaba/smart/framework/engine/instance/impl/DefaultProcessInstance.java
@@ -66,7 +66,7 @@ public class DefaultProcessInstance extends AbstractLifeCycleInstance implements
             if (executionInstance.isAbort()) {
                 data.append("abort");
             }else if (executionInstance.isEnd()) {
-                data.append("end");
+                return "1|theEnd|^";
             }else {
                 data.append(executionInstance.toDatabase());
             }
