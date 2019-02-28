8711389a92a4c3213bac7307330e5ad2fbe16cc7
zeyuan.cgd <zeyuan.cgd@alibaba-inc.com>
2019-02-28 11:35:34 +0800

    行级别审批

---
+++ api/src/main/java/com/alibaba/smart/framework/engine/constant/ProcessInstanceMode.java
@@ -0,0 +1,17 @@
+package com.alibaba.smart.framework.engine.constant;
+
+/**
+ * 流程实例模式
+ */
+public interface ProcessInstanceMode {
+
+    /**
+     * 正常模式
+     */
+    String NORMAL = "normal";
+
+    /**
+     * 行审批模式
+     */
+    String ITEM = "item";
+}
