7b863b014541a673c53165b96bc57f2c3b3c58b9
haijun.ghj <haijun.ghj@alibaba-inc.com>
2019-10-16 21:20:20 +0800

    refactor model relationship

--- api/src/main/java/com/alibaba/smart/framework/engine/model/assembly/impl/AbstractProcess.java
+++ api/src/main/java/com/alibaba/smart/framework/engine/model/assembly/impl/AbstractProcess.java
@@ -1,20 +1,20 @@
-package com.alibaba.smart.framework.engine.model.assembly.impl;
-
-import java.util.List;
-
-import com.alibaba.smart.framework.engine.model.assembly.BaseElement;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-/**
- * @author 高海军 帝奇  2016.11.11
- * @author ettear 2016.04.13
- */
-@Data
-@EqualsAndHashCode(callSuper = true)
-public abstract class AbstractProcess extends AbstractActivity implements com.alibaba.smart.framework.engine.model.assembly.Process {
-
-    private static final long serialVersionUID = -1139915200717778375L;
-    private List<BaseElement> elements;
-}
+//package com.alibaba.smart.framework.engine.model.assembly.impl;
+//
+//import java.util.List;
+//
+//import com.alibaba.smart.framework.engine.model.assembly.BaseElement;
+//
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//
+///**
+// * @author 高海军 帝奇  2016.11.11
+// * @author ettear 2016.04.13
+// */
+//@Data
+//public abstract class AbstractProcess  implements com.alibaba.smart.framework.engine.model.assembly.Process {
+//
+//    private static final long serialVersionUID = -1139915200717778375L;
+//
+//
+//}
--- bpmn/src/main/java/com/alibaba/smart/framework/engine/modules/bpmn/assembly/process/Process.java
+++ bpmn/src/main/java/com/alibaba/smart/framework/engine/modules/bpmn/assembly/process/Process.java
@@ -1,17 +1,26 @@
 package com.alibaba.smart.framework.engine.modules.bpmn.assembly.process;
 
+import java.util.List;
+
 import javax.xml.namespace.QName;
 
-import com.alibaba.smart.framework.engine.model.assembly.impl.AbstractProcess;
+import com.alibaba.smart.framework.engine.model.assembly.BaseElement;
 import com.alibaba.smart.framework.engine.modules.bpmn.constant.BpmnNameSpaceConstant;
+
+import lombok.Data;
 
 /**
  * @author 高海军 帝奇 Apr 21, 2016 1:37:18 PM TODO 这个需要考虑下,process 自身不是一个activity,但是subprocess 被当成一个activity
  */
-public class Process extends AbstractProcess {
+@Data
+public class Process implements com.alibaba.smart.framework.engine.model.assembly.Process {
 
     public final static QName type = new QName(BpmnNameSpaceConstant.NAME_SPACE, "process");
 
     private static final long serialVersionUID = -2660788294142169268L;
 
+    private String id;
+    private String name;
+
+    private List<BaseElement> elements;
 }
