5623af41288310d9b5c2b977502280d25ce0580b
haijun.ghj <haijun.ghj@alibaba-inc.com>
2019-12-01 19:36:12 +0800

    clean code

--- base/src/main/java/com/alibaba/smart/framework/engine/xml/exception/ParseException.java
+++
@@ -1,9 +0,0 @@
-//package com.alibaba.smart.framework.engine.xml.exception;
-//
-///**
-// * Created by 高海军 帝奇 74394 on  2019-08-24 17:43.
-// */
-//public class ParseException extends Exception {
-//    //fixme
-//
-//}
