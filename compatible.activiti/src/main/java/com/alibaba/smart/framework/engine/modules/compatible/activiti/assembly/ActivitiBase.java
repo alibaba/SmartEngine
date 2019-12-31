b952427404ac2b94031cef11b6ae1db918ba6ca5
haijun.ghj <haijun.ghj@alibaba-inc.com>
2019-12-31 21:43:28 +0800

    clean code

--- compatible.activiti/src/main/java/com/alibaba/smart/framework/engine/modules/compatible/activiti/assembly/ActivitiBase.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.smart.framework.engine.modules.compatible.activiti.assembly;
-
-/**
- * @author ettear
- * Created by ettear on 15/10/2017.
- */
-public abstract class ActivitiBase {
-    public static final String NAME_SPACE="http://activiti.org/bpmn";
-}
