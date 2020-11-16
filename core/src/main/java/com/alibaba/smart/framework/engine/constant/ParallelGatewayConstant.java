8ba187aade68fa43783709cb30376f6c74e86d19
yfcchilly <yfcchilly@126.com>
2020-11-16 20:04:11 +0800

    [OPT]add wait timeout for parallel gateway

---
+++ core/src/main/java/com/alibaba/smart/framework/engine/constant/ParallelGatewayConstant.java
@@ -0,0 +1,21 @@
+package com.alibaba.smart.framework.engine.constant;
+
+/**
+ * 并行网关扩展属性常量
+ *
+ * @author guoxing
+ * @date 2020年11月16日19:29:08
+ */
+public final class ParallelGatewayConstant {
+
+   /**
+    * private scope constructor for avoid new instance
+    */
+   private ParallelGatewayConstant() {
+   }
+
+   /**
+    * 自定义属性名：等待超时时间，单位毫秒。
+    */
+   public static final String WAIT_TIME_OUT = "timeout";
+}
