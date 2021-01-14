7114238eeb4480e36b91294db8b6350bcfc3fed9
yfcchilly <yfcchilly@126.com>
2021-01-14 20:38:43 +0800

    并行网关增加race模式，支持anyof

--- core/src/main/java/com/alibaba/smart/framework/engine/constant/ParallelGatewayConstant.java
+++ core/src/main/java/com/alibaba/smart/framework/engine/constant/ParallelGatewayConstant.java
@@ -1,5 +1,7 @@
 package com.alibaba.smart.framework.engine.constant;
 
+import lombok.Getter;
+
 /**
  * 并行网关扩展属性常量
  *
@@ -27,5 +29,55 @@ public final class ParallelGatewayConstant {
     */
    public static final String SKIP_TIMEOUT_EXCEPTION = "skipTimeoutExp";
 
+   /**
+    * 执行策略，value为code对应的数字
+    */
+   public static final String EXE_STRATEGY = "strategy";
 
+
+   public enum ExecuteStrategy {
+      /**
+       * 执行所有
+       */
+      INVOKE_ALL("all", "执行所有"),
+      /**
+       * 返回最快的那个，中断其他进行中的并行线程
+       */
+      INVOKE_ANY("any", "返回最快的任意一个")
+      ;
+
+      /**
+       * 1 表示执行所有
+       * 2 表示执行最快的那个，类似race模式
+       */
+      @Getter
+      private final String code;
+      /**
+       * 策略的描述
+       */
+      @Getter
+      private final String desc;
+
+      ExecuteStrategy(String code, String desc) {
+         this.code = code;
+         this.desc = desc;
+      }
+
+
+      /**
+       * 返回一个执行策略
+       * @param code
+       * @return
+       */
+      public static ExecuteStrategy build(String code) {
+         ExecuteStrategy[] strategies = ExecuteStrategy.values();
+         for (ExecuteStrategy strategy : strategies) {
+            if (strategy.getCode().equalsIgnoreCase(code)) {
+               return strategy;
+            }
+         }
+         return null;
+      }
+
+   }
 }
